04362d6a999386c30553ba521505e6dcdf05360f
neo2001-cross <ngochuy11010@gmail.com>
2023-03-31 23:56:33 +0700

    OEE Remaster

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import './App.css';
-import Header from './components/Header';
-import Sidebar from './components/Sidebar';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Dashboard from "./pages/Dashboard"
 import DeviceReport from './pages/DeviceReport'
@@ -11,9 +9,7 @@ function App() {
   return (
   <Router>
     <div className="App">
-      <Header />
       <div className='mainApp'>
-        <Sidebar />
         <Routes>
           <Route path='/' element={<Dashboard />} />
           <Route path='/devices' element={<DeviceReport />}/>
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,13 +1,7 @@
-import "../assets/styles/Dashboard.scss"
-import GeneralOEE from "../components/GeneralOEE"
-import DetailOEE from "../components/DetailOEE"
 
 function Dashboard(){
   return(
-    <div className="Dashboard">
-      <GeneralOEE />
-      <DetailOEE />
-    </div>
+    <div></div> 
   )
 }
 
